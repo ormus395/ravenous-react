32f8e2ddd6f609297a77327746068f7f835b7bc7
ormus395 <ormus395@gmail.com>
2020-05-27 02:22:46 -0700

    add finished search bar and home page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 
+import SearchBar from "../SearchBar/SearchBar";
 import BusinessList from "../BusinessList/BusinessList";
 
 function App() {
   return (
     <div className="App">
+      <SearchBar />
       <BusinessList />
     </div>
   );
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,36 @@
 import React from "react";
 import "./SearchBar.css";
 
+const sortByOptions = {
+  "Best Match": "best_match",
+  "Highest Rated": "highest_rated",
+  "Most Reviewed": "most_reviewed",
+};
+
 class SearchBar extends React.Component {
+  renderSoryByOptions() {
+    return Object.keys(sortByOptions).map((option) => {
+      let sortBy = sortByOptions[option];
+      return <li key={sortBy}>{option}</li>;
+    });
+  }
+
   render() {
-    return <div></div>;
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>{this.renderSoryByOptions()}</ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+    );
   }
 }
+
+export default SearchBar;
