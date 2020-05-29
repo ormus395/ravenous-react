38e4c3d14ddce15f4b4ee5d5070ea61de619fe64
ormus395 <ormus395@gmail.com>
2020-05-29 14:07:51 -0700

    Add dynamic functionality using props, instead of hard coded vars in components

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,14 +5,11 @@ import Business from "../Business/Business";
 
 class BusinessList extends React.Component {
   render() {
-    return (
-      <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-      </div>
-    );
+    let businesses = this.props.businesses.map((business) => {
+      return <Business business={business} />;
+    });
+
+    return <div className="BusinessList">{businesses}</div>;
   }
 }
 
